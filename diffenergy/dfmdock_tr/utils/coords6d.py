0f13da44e61554e29c57965fe5d585524cbc3e4e
dxu16 <xuda1996@gmail.com>
2025-04-24 10:35:58 -0400

    fix coords6d

--- diffenergy/dfmdock_tr/utils/coords6d.py
+++ diffenergy/dfmdock_tr/utils/coords6d.py
@@ -29,11 +29,11 @@ def calc_dihedral(a_coords,
     b2 = b_coords - c_coords
     b3 = c_coords - d_coords
 
-    n1 = torch.cross(b1, b2)
+    n1 = torch.linalg.cross(b1, b2)
     n1 = torch.div(n1, n1.norm(dim=-1, keepdim=True))
-    n2 = torch.cross(b2, b3)
+    n2 = torch.linalg.cross(b2, b3)
     n2 = torch.div(n2, n2.norm(dim=-1, keepdim=True))
-    m1 = torch.cross(n1, torch.div(b2, b2.norm(dim=-1, keepdim=True)))
+    m1 = torch.linalg.cross(n1, torch.div(b2, b2.norm(dim=-1, keepdim=True)))
 
     dihedral = torch.atan2((m1 * n2).sum(-1), (n1 * n2).sum(-1))
 
