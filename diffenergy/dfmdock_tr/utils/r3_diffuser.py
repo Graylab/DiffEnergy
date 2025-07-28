14e75c74cefc65100aa9305efc9dc2518145bfe5
dxu16 <xuda1996@gmail.com>
2025-07-28 08:54:49 -0400

    change dfmdock to ori

--- diffenergy/dfmdock_tr/utils/r3_diffuser.py
+++ diffenergy/dfmdock_tr/utils/r3_diffuser.py
@@ -1,22 +1,26 @@
-"""R^3 diffusion methods."""
+# The code is adopted from:
+# https://github.com/jasonkyuyim/se3_diffusion/blob/master/data/r3_diffuser.py
+
 import numpy as np
 import torch
 
+#----------------------------------------------------------------------------
+# Helper functions
+
 move_to_np = lambda x: x.cpu().detach().numpy()
 
-class R3Diffuser:
-    """VE-SDE diffuser class for translations."""
+#----------------------------------------------------------------------------
+# VE-SDE diffuser class for diffusion in 3D translational (R(3)) space 
 
+class R3Diffuser:
     def __init__(self, conf):
         self.min_sigma = conf.min_sigma
         self.max_sigma = conf.max_sigma
 
     def sigma(self, t):
-        #return self.min_sigma * (self.max_sigma / self.min_sigma) ** t
-        return self.min_sigma ** (1 - t) * self.max_sigma ** t
+        return self.min_sigma * (self.max_sigma / self.min_sigma) ** t
 
     def diffusion_coef(self, t):
-        """Time-dependent diffusion coefficient."""
         return self.sigma(t) * np.sqrt(2 * (np.log(self.max_sigma) - np.log(self.min_sigma))) 
 
     def torch_score(self, tr_t, t):
@@ -26,17 +30,8 @@ class R3Diffuser:
         return 1 / self.sigma(t)
 
     def forward_marginal(self, t: float):
-        """Samples marginal p(x(t) | x(0)).
-        Args:
-            t: continuous time in [0, 1].
-            x0: ligand center [..., 3]
-        Returns:
-            tr_t: [..., 3] positions at time t in Angstroms.
-            tr_score: [..., 3] score at time t in scaled Angstroms.
-        """
         if not np.isscalar(t):
             raise ValueError(f'{t} must be a scalar.')
-
         z = np.random.randn(1, 3)
         tr_t = self.sigma(t) * z 
         tr_score = self.torch_score(tr_t, t)
@@ -48,21 +43,14 @@ class R3Diffuser:
             dt: torch.tensor,
             t: float,
             noise_scale: float=1.0,
+            ode: bool=False,
         ):
-        """Simulates the reverse SDE for 1 step
-        Args:
-            score_t: [..., 3] rotation score at time t.
-            t: continuous time in [0, 1].
-            dt: continuous step size in [0, 1].
-            mask: True indicates which residues to diffuse.
-        Returns:
-            [..., 3] positions at next step t-1.
-        """
         if not np.isscalar(t): raise ValueError(f'{t} must be a scalar.')
-
         g_t = self.diffusion_coef(t)
-        z = noise_scale * torch.randn(1, 3, device=score_t.device)
-        perturb = (g_t ** 2) * score_t * dt + g_t * torch.sqrt(dt) * z
-
+        if not ode:
+            z = noise_scale * torch.randn(1, 3, device=score_t.device)
+            perturb = (g_t ** 2) * score_t * dt + g_t * torch.sqrt(dt) * z
+        else:
+            perturb = 0.5 * (g_t ** 2) * score_t * dt
         return perturb.float()
 
