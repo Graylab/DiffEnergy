43d0468909d0c23b55a311e025e87bd7df1778b3
minerharry <miner.harry567@gmail.com>
2025-09-25 14:49:45 -0400

    data translation script

--- scripts/batch_likelihoodv3_gt.sh
+++ scripts/batch_likelihoodv3_gt.sh
@@ -1,7 +1,8 @@
 #python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_flow.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_flow.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_diff.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff_interp.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_flow.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_flow.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_diff.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff_interp.yaml && \
+python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_translation.yaml && \
 echo "all likelihoods computed successfully!"
