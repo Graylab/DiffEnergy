37a00092cc67624b2b387a667e52b8e8e9b0a316
htruscott <harrison@harriscott.net>
2025-09-25 18:02:14 -0400

    added diffusion translation trajectory

--- scripts/batch_likelihoodv3_gt.sh
+++ scripts/batch_likelihoodv3_gt.sh
@@ -4,5 +4,5 @@
 # python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_flow.yaml && \
 # python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_linearized_diff.yaml && \
 # python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff_interp.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_translation.yaml && \
+python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_gtscore_diff_translation.yaml && \
 echo "all likelihoods computed successfully!"
