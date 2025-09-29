083b8796ef90c34722d75730340b8697a0fb0880
htruscott <harrison@harriscott.net>
2025-09-29 16:25:23 -0400

    fixed slurm scripts

--- scripts/sbatch_likelihoodv3_srun.sh
+++ scripts/sbatch_likelihoodv3_srun.sh
@@ -1,11 +1,11 @@
 #bash sbatch_likelihood_v3_template_srun.sh likelihood_gaussian_1d_
 
 #NN
-bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_flow.yaml               '++num_gpus=4'&
-bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_diff.yaml               '++num_gpus=4'&
-bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_linearized_flow.yaml    '++num_gpus=4'&
-bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_linearized_diff.yaml    '++num_gpus=4'&
-bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_diff_interp.yaml        '++num_gpus=4'&
+bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_flow.yaml               &
+bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_diff.yaml               &
+bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_linearized_flow.yaml    &
+bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_linearized_diff.yaml    &
+bash scripts/sbatch_likelihoodv3_template_srun_gpu.sh likelihood_gaussian_1d_3integrand_diff_interp.yaml        &
 
 #GT
 bash scripts/sbatch_likelihoodv3_template_srun.sh likelihood_gaussian_1d_gtscore_flow.yaml                      &
--- scripts/sbatch_likelihoodv3_template_srun_gpu.sh
+++ scripts/sbatch_likelihoodv3_template_srun_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-name = $1
+name=$1
 shift
-srun --job-name=diffenergy_$name --partition=a100 --account=jgray21_gpu --gpus_per_task=4 --time=08:00:00 -o logs/$name-log.txt bash -c "module load anaconda; conda activate diffenergy; python scripts/likelihoodv3_gaussian_1d.py --config-name=$name" $@
+srun --job-name=diffenergy_$name --partition=a100 --account=jgray21_gpu --time=08:00:00 -o logs/$name-log.txt bash -c "module load anaconda; conda activate diffenergy; python scripts/likelihoodv3_gaussian_1d.py --config-name=$name" $@
