28fee138713b84a02e8fd8a21c9b52b6beb6d5ad
minerharry <miner.harry567@gmail.com>
2025-10-05 15:17:35 -0400

    score model evaluator for dfmdock, changed the score model wrapper to impose translation/rotations based on rotation and translation vectors

--- diffenergy/dfmdock_tr/likelihood_helpers.py
+++ diffenergy/dfmdock_tr/likelihood_helpers.py
@@ -1,7 +1,7 @@
 
 
 import abc
-from typing import Generic, Iterable, Optional, Protocol, Sequence, TypeVar, TypedDict
+from typing import Generic, Iterable, Literal, Optional, Protocol, Sequence, TypeVar, TypeVarTuple, TypedDict
 
 import IPython
 from torch import FloatTensor, Tensor
@@ -14,117 +14,119 @@ class DFMDict(TypedDict):
     rec_x: Tensor
     lig_x: Tensor
     rec_pos: Tensor
-    lig_pos: Tensor
+    lig_pos_orig: Tensor #the ORIGINAL POSITION of the ligand. Should be centered at zero!
     position_matrix: Tensor
-    t: Tensor
 
 class LigDict(TypedDict):
-    lig_pos: Tensor
+    offset: Tensor #either a 3d (Translation or Rotation) or 6d (Translation + Rotation) vector representing how to transform lig_pos in space
     
 # not gonna deal with shape nonsense, just gonna keep everything in its default (batched?) shape.
 # all the likelihood stuff can use arrays of any shape so it's fine
 def to_array(x:LigDict)->Tensor:
-    return x['lig_pos']
+    return x['offset']
 def from_array(a,device:str|torch.device='cuda')->LigDict:
-    return {'lig_pos':torch.as_tensor(a,dtype=torch.float,device=torch.device(device))}
+    return {'offset':torch.as_tensor(a,dtype=torch.float,device=torch.device(device))}
 
 
-X = TypeVar("X")
-def getcache(x:Tensor,t:float,cache:Optional[tuple[tuple[Tensor,float],X]])->Optional[X]:
-    if cache and (x is cache[0][0]) and t == cache[0][1]: #don't bother checking tensor equality - assume it never comes up (shouldn't)
+X = TypeVarTuple("X")
+R = TypeVar("R")
+def getcache(x:tuple[*X],t:float,cache:Optional[tuple[tuple[*X,float],R]])->Optional[R]:
+    if cache and all(x[i] is cache[0][i] for i in range(len(x))) and t == cache[0][-1]: #don't bother checking tensor equality - assume it never comes up (shouldn't)
         return cache[1]
     return None
 
 
 class ModelEval(ScoreModelEvaluator[LigDict,DFMDict]): #unbatched has a size of 1 in first dim, batched has size of N
-    def __init__(self,score_model:Score_Model, always_grad:bool=True) -> None:
+    def __init__(self,score_model:Score_Model, offset_type:Literal["Translation","Rotation","Translation+Rotation"], always_grad:bool=True) -> None:
         self.score_model = score_model
-        self.scorecache: Optional[tuple[tuple[Tensor,float],Tensor]] = None
-        self.divcache: Optional[tuple[tuple[Tensor,float],Tensor]] = None
+        self.scorecache: Optional[tuple[tuple[LigDict,DFMDict,float],Tensor]] = None
+        self.divcache: Optional[tuple[tuple[LigDict,DFMDict,float],Tensor]] = None
         self.always_grad = always_grad
         self.dtype = self.score_model.parameters()
+        self.offset_type = offset_type
 
 
-    def batch_score(self,batch:Tensor,t:float,conditioning:None,grad:bool=False):
-        cache = getcache(batch,t,self.scorecache)
+    def score(self,x:LigDict,t:float,conditioning:DFMDict,grad:bool=False):
+        batch = x
+
+        cache = getcache((batch,conditioning),t,self.scorecache)
         if cache is not None: return cache
         
-        x = batch
+        offset = batch["offset"]
+
+        assert offset.ndim == 1 #no batch support yet
 
         # enable grad to cache the gradients
         if self.always_grad or grad:
             grad_ctx = torch.enable_grad
-            x.requires_grad_(True)
+            offset.requires_grad_(True)
         else:
             grad_ctx = torch.no_grad
-            x.requires_grad_(False)
+            offset.requires_grad_(False)
+
+
+        offset_tr = None; offset_rot = None
+
+        if self.offset_type == "Translation+Rotation":
+            offset_tr,offset_rot = offset[...,:3],offset[...,3:]
+        elif self.offset_type == "Translation":
+            offset_tr = offset
+        elif self.offset_type == "Rotation":
+            offset_rot = offset
+        else:
+            raise ValueError(self.offset_type)
+
+
+        new_batch = dict(**conditioning)
+        new_batch["offset_tr"] = offset_tr
+        new_batch["offset_rot"] = offset_rot
+
+        new_batch["t"] = torch.as_tensor(t,device=offset.device,dtype=offset.dtype)
+        
 
         with grad_ctx(): #not sure if this actually is that important, might be enough to just set requires grad to true/false. don't think it can hurt, though
-            batchscore = self.score_model(x, torch.as_tensor([t],device=x.device,dtype=x.dtype).expand((x.shape[0],)))
+            scores = self.score_model(new_batch)
 
-        self.scorecache = ((x,t),batchscore)
-        return batchscore
-    
+        tr_score:Tensor = scores["tr_score"]; rot_score:Tensor = scores["rot_score"]
 
-    def score(self,x:Tensor,t:float,conditioning:None,grad:bool=False):
-        score = self.batch_score(x,t,conditioning,grad=grad)
-        if score.shape[0] == 1:
-            return score[0]
+        score = None
+        if self.offset_type == "Translation+Rotation":
+            score = torch.cat((tr_score,rot_score))
+        elif self.offset_type == "Translation":
+            score = tr_score
+        elif self.offset_type == "Rotation":
+            score = rot_score
         else:
-            raise ValueError("x must have size 1 in dimension 1 to use unbatched score! Noneall batch_score otherwise!")
+            raise ValueError(self.offset_type)
+
+        self.scorecache = ((batch,conditioning,t),score)
+        return score
     
-    def batch_divergence(self,batch:Tensor,t:float,conditioning:None)->Tensor:
-        cache = getcache(batch,t,self.divcache)
+
+    def divergence(self,x:LigDict,t:float,conditioning:DFMDict)->Tensor:
+        batch = x
+
+        cache = getcache((batch,conditioning),t,self.divcache)
         if cache is not None: return cache
-
-        x = batch
         
-        batchscore = getcache(x,t,self.scorecache)
-        if batchscore is None:
+        score = getcache((batch,conditioning),t,self.scorecache)
+        if score is None:
             self.scorecache = None #make sure to invalidate a bad cache
-            batchscore = self.batch_score(x,t,conditioning,grad=True)
+            score = self.score(batch,t,conditioning,grad=True)
 
-        grad_scores = torch.empty(x.shape,dtype=x.dtype,device=x.device)
-        for b in range(x.shape[0]):
-            for i in range(x.shape[1]):
-                grad_scores[b,i] = torch.autograd.grad(batchscore[b,i], x, retain_graph=True, create_graph=False)[0][b,i]
+        offset = batch['offset']
+
+        assert offset.ndim == 1
+
+        grad_scores = torch.empty(offset.shape,dtype=offset.dtype,device=offset.device)
+        for i in range(offset.shape[0]):
+            grad_scores[i] = torch.autograd.grad(score[i], offset, retain_graph=True, create_graph=False)[0][i]
         batchtrace = torch.sum(grad_scores, dim=1)
 
-        self.divcache = ((x,t),batchtrace)
+        self.divcache = ((batch,conditioning,t),batchtrace)
 
         return batchtrace #essentially a float teehee
 
-    def divergence(self,x:Tensor,t:float,conditioning:None)->float:
-        div = self.batch_divergence(x,t,conditioning)
-        if div.shape[0] == 1:
-            return div[0]
-        else:
-            raise ValueError("x must have size 1 in dimension 1 to use unbatched divergence! Call batch_divergence otherwise!")
-        
-
-        
-def divergence_eval_tr(batch, score_model:Score_Model):
-    # Compute the divergence of the score-based model
-
-    # grab some input
-    lig_pos = batch["lig_pos"]
-
-    with torch.enable_grad():
-        lig_pos.requires_grad_(True)
-        tr_score = score_model(batch)["tr_score"].squeeze()
-
-        # Compute the gradient of tr_score w.r.t lig_pos
-        grad_score_lig = []
-        for i in range(tr_score.shape[0]):
-            grad = torch.autograd.grad(tr_score[i], lig_pos, retain_graph=True, create_graph=False)[0]
-            grad_score_lig.append(grad)
-        grad_score_lig = torch.stack(grad_score_lig, dim=0)  # shape[3,107,3,3]
-        grad_score_lig_ca = grad_score_lig[:,:,1,:].squeeze() # shape[3,107,3]
-        grad_score_lig_ca_mean = grad_score_lig_ca.mean(dim=1).squeeze() # shape[3,3]
-        diagonal = torch.diagonal(grad_score_lig_ca_mean)
-        trace = torch.sum(diagonal).unsqueeze(0)
-
-    return trace
 
     
 
