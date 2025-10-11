b894e31244d9994b4034b930e17639a8bf141d20
htruscott <harrison@harriscott.net>
2025-10-11 16:51:07 -0400

    config and script updates

---
+++ scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+name=$1
+shift
+srun --job-name=diffenergy_$name --qos=qos_gpu --gres=gpu:1 --partition=a100,ica100,v100 --account=jgray21_gpu --time=72:00:00 -o logs/$name-log.txt bash -c "module load anaconda; conda activate diffenergy; python scripts/likelihoodv3_dfmdock.py --config-name=$name $@"
