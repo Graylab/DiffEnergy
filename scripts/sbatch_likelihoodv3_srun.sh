595dca74254064f028347737a50c76716b36dec1
htruscott <harrison@harriscott.net>
2025-10-14 15:40:39 -0400

    configs n scripts

--- scripts/sbatch_likelihoodv3_srun.sh
+++ scripts/sbatch_likelihoodv3_srun.sh
@@ -1,15 +1,16 @@
 #bash sbatch_likelihood_v3_template_srun.sh likelihood_
 
 #dfmdock_tr - NN
-# bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_flow.yaml               &
-bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_diff.yaml               '++overwrite_output=True' &
+# bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_flow_40.yaml               '++overwrite_output=True' &
+# bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_diff.yaml               '++overwrite_output=True' &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_linearized_flow.yaml    &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_linearized_diff.yaml    &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_diff_interp.yaml        &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_3integrand_diff_50interp.yaml      &
 
 #dfmdock_tr_2SIC - NN
-# bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_flow.yaml              '++overwrite_output=True' &
+bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_flow.yaml            &
+# bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_flow_40.yaml              '++overwrite_output=True' &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_diff.yaml              '++overwrite_output=True' &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_linearized_flow.yaml    &
 # bash scripts/sbatch_likelihoodv3_template_srun_dfmdock_gpu.sh likelihood_dfmdock_tr_2SIC_3integrand_linearized_diff.yaml   '++overwrite_output=True' &
