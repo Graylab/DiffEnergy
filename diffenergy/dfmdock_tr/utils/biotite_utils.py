ca9ab42dbb7de93a6f6bc8a04faf44579d320373
htruscott <harrison@harriscott.net>
2025-11-03 23:34:08 -0500

    refactor biotite utils

--- diffenergy/dfmdock_tr/utils/biotite_utils.py
+++ diffenergy/dfmdock_tr/utils/biotite_utils.py
@@ -1,9 +1,15 @@
 from pathlib import Path
+from typing import Iterable
 from biotite.structure.io.general import load_structure, save_structure
 from biotite.structure import AtomArray, get_chains
+try:
+    from biotite.structure import filter_peptide_backbone
+except ImportError:
+    from biotite.structure import filter_backbone as filter_peptide_backbone
 import numpy as np
 import torch
 from diffenergy.dfmdock_tr.utils.geometry import axis_angle_to_matrix
+from diffenergy.dfmdock_tr.utils.esm_utils import extract_coords_from_structure
 
 def modify_aa_coords(x, rot, tr):
     center = x.mean(axis=0)
@@ -12,19 +18,50 @@ def modify_aa_coords(x, rot, tr):
     x = x + tr.cpu().numpy()
     return x
 
+def get_chain_structure(orig:str|Path|AtomArray,chain:str,backbone_only:bool=False):
+    if isinstance(orig,AtomArray):
+        orig_structure = orig
+    else:
+        orig_structure:AtomArray = load_structure(orig)
+        
+    all_chains = get_chains(orig_structure)
+    if len(all_chains) == 0:
+        raise ValueError("No chains found in the input.")
+    elif chain not in all_chains:
+        raise ValueError(f"Cannot offset chain {chain}; not in input!")
+    
+    if backbone_only:
+        bbmask = filter_peptide_backbone(orig_structure)
+        orig_structure = orig_structure[bbmask]
+
+    #we can extract just the ligands, modify the structure, then assign it back using this boolean mask
+    lig_filter = orig_structure.chain_id == chain #get boolean mask
+    lig_structure = orig_structure[lig_filter]
+    
+    return lig_structure
+
+#same as load_coords from esm_utils, but can take an already loaded atomarray as input to save disk read
+def get_chain_coords(orig:str|Path|AtomArray,chain:str,backbone_only:bool=True):
+    return extract_coords_from_structure(get_chain_structure(orig,chain,backbone_only=backbone_only))
+
+
 # load pdb with **all** atoms, not just backbone atoms, and offset specified chain. Defaults to B cause that's the default ligand chain
 def get_offset_pdb(
-        orig:str|Path,
+        orig:str|Path|AtomArray,
         offset_tr:None|torch.Tensor,
         offset_rot:None|torch.Tensor,
         offset_chain="B"
         ):
-    orig_structure:AtomArray = load_structure(orig)
+    if isinstance(orig,AtomArray):
+        orig_structure = orig
+    else:
+        orig_structure:AtomArray = load_structure(orig)
+        
     all_chains = get_chains(orig_structure)
     if len(all_chains) == 0:
-        raise ValueError("No chains found in the input file.")
+        raise ValueError("No chains found in the input.")
     elif offset_chain not in all_chains:
-        raise ValueError(f"Cannot offset chain {offset_chain}; not in file!")
+        raise ValueError(f"Cannot offset chain {offset_chain}; not in input!")
 
     #we can extract just the ligands, modify the structure, then assign it back using this boolean mask
     lig_filter = orig_structure.chain_id == offset_chain #get boolean mask
