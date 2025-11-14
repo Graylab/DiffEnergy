86b15b8ca5d859f2a906fa55ad1dc129db9e7b8d
htruscott <harrison@harriscott.net>
2025-11-14 15:12:42 -0500

    scrip

--- scripts/batch_likelihoodv3.sh
+++ scripts/batch_likelihoodv3.sh
@@ -1,7 +1,9 @@
-#python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_3integrand_flow.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_3integrand_diff.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_3integrand_linearized_flow.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_3integrand_linearized_diff.yaml && \
-python scripts/likelihoodv3_gaussian_1d.py --config-name=likelihood_gaussian_1d_3integrand_diff_interp.yaml && \
+#python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_flow.yaml && \
+python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_diff_interp_all_dum.yaml && \
+python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_diff_interp_all_trapezoid.yaml && \
+python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_diff_interp_all_backwards.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_linearized_flow.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_linearized_diff.yaml && \
+# python scripts/likelihoodv3_gaussian_1d.py --config-name=gaussian_1d/3integrand_diff_interp.yaml && \
 echo "all likelihoods computed successfully!"
