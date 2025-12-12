e13335641c2af6f6c9ae81dfa477306fbc18f8d2
htruscott <harrison@harriscott.net>
2025-12-12 16:46:29 -0500

    scrip

--- user_scripts/test_classes.sh
+++ user_scripts/test_classes.sh
@@ -3,7 +3,8 @@
 # python scripts/get_dfmdock_forces.py --config-name=testconf/dfmdock_traj_forces_2A1A.yaml   &
 # python scripts/sample_dfmdock.py --config-name=testconf/sample_dfmdock_nonoise.yaml   &
 
-python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_diff_trapezoid.yaml   &
-# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_flow.yaml   &
+# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_diff_trapezoid.yaml ++overwrite_output=True   &
+# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_flow.yaml ++overwrite_output=True   &
 # python scripts/get_gaussian_forces.py --config-name=testconf/gaussian_traj_forces.yaml   &
 # python scripts/sample_gaussian_1d.py --config-name=testconf/sample_gaussian_1d.yaml   &
+wait
