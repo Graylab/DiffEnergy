f674a7facdbe74a094f30f2677715e4d169b19b2
htruscott <harrison@harriscott.net>
2025-12-15 13:32:03 -0500

    remove divide div by N

--- diffenergy/dfmdock_tr/likelihood_helpers.py
+++ diffenergy/dfmdock_tr/likelihood_helpers.py
@@ -48,8 +48,7 @@ class DFMDockModelEval(CachedScoreModelEvaluator[LigDict,DFMDict]): #unbatched h
                  offset_type:Literal["Translation","Rotation","Translation+Rotation"], 
                  always_grad:bool=True,
                  reset_seed_each_eval:bool=False,
-                 manual_seed:int=0,
-                 divide_div_by_N=False) -> None:
+                 manual_seed:int=0) -> None:
         
         super().__init__()
 
@@ -64,8 +63,6 @@ class DFMDockModelEval(CachedScoreModelEvaluator[LigDict,DFMDict]): #unbatched h
 
         self.reset_seed_each_eval = reset_seed_each_eval
         self.manual_seed = manual_seed
-
-        self.divide_div_by_N = divide_div_by_N
 
 
     def score(self, x:LigDict, t:float, conditioning:DFMDict, grad:bool=False, return_grad:bool=False):
@@ -128,8 +125,6 @@ class DFMDockModelEval(CachedScoreModelEvaluator[LigDict,DFMDict]): #unbatched h
                 for i in range(offset.shape[0]):
                     grad_scores[i] = torch.autograd.grad(score[i], offset, retain_graph=True, create_graph=False)[0][i]
             trace = torch.sum(grad_scores, dim=0)
-            if self.divide_div_by_N:
-                trace = trace/conditioning["lig_pos_orig"].shape[0] #divide by N
 
             self._put_divergence(x,t,conditioning,trace)
 
