ec249b818d06fa86bb710e37f4545916b6c71da1
htruscott <harrison@harriscott.net>
2025-12-15 14:13:45 -0500

    remove user scripts and train results

--- user_scripts/batch_likelihoodv3_dfmdock_2SIC.sh
+++
@@ -1,7 +0,0 @@
-#python scripts/likelihood_gaussian_1d.py --config-name=likelihood_gaussian_1d_
-# python scripts/likelihood_dfmdock.py --config-name=likelihood_dfmdock_tr_2SIC_3integrand_flow.yaml && \
-python scripts/likelihood_dfmdock.py --config-name=likelihood_dfmdock_tr_2SIC_3integrand_diff.yaml && \
-# python scripts/likelihood_dfmdock.py --config-name=likelihood_dfmdock_tr_2SIC_3integrand_linearized_flow.yaml && \
-# python scripts/likelihood_dfmdock.py --config-name=likelihood_dfmdock_tr_2SIC_3integrand_linearized_diff.yaml && \
-# python scripts/likelihood_dfmdock.py --config-name=likelihood_dfmdock_tr_2SIC_3integrand_diff_interp.yaml && \
-echo "all likelihoods computed successfully!"
