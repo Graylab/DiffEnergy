921978125d2c1aee54c22cd32968cc7fc7e21783
htruscott <harrison@harriscott.net>
2025-12-14 22:35:23 -0500

    tests passed

--- user_scripts/test_classes.sh
+++ user_scripts/test_classes.sh
@@ -1,10 +1,9 @@
-# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_diff_trapezoid.yaml   &
-# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_flow_40.yaml   &
-# python scripts/get_dfmdock_forces.py --config-name=testconf/dfmdock_traj_forces_2A1A.yaml   &
-# python scripts/sample_dfmdock.py --config-name=testconf/sample_dfmdock_nonoise.yaml   &
+# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_diff_trapezoid.yaml ++overwrite_output=True
+# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_flow_40.yaml ++overwrite_output=True
+# python scripts/get_dfmdock_forces.py --config-name=testconf/dfmdock_traj_forces_2A1A.yaml ++overwrite_output=True
+# python scripts/sample_dfmdock.py --config-name=testconf/sample_dfmdock_nonoise.yaml ++overwrite_output=True
 
-# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_diff_trapezoid.yaml ++overwrite_output=True   &
-# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_flow.yaml ++overwrite_output=True   &
-# python scripts/get_gaussian_forces.py --config-name=testconf/gaussian_traj_forces.yaml   &
-# python scripts/sample_gaussian_1d.py --config-name=testconf/sample_gaussian_1d.yaml   &
-wait
+# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_diff_trapezoid.yaml ++overwrite_output=True
+# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_flow.yaml ++overwrite_output=True
+# python scripts/get_gaussian_forces.py --config-name=testconf/gaussian_traj_forces.yaml ++overwrite_output=True
+python scripts/sample_gaussian_1d.py --config-name=testconf/sample_gaussian_1d.yaml ++overwrite_output=True
