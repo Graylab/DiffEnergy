acc5a71c01cf1f40c3f06571f80c24b9378c0798
htruscott <harrison@harriscott.net>
2025-12-15 14:13:45 -0500

    remove user scripts and train results

--- user_scripts/test_classes.sh
+++
@@ -1,9 +0,0 @@
-# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_diff_trapezoid.yaml ++overwrite_output=True
-# python scripts/likelihood_dfmdock.py --config-name=testconf/dfmdock_flow_40.yaml ++overwrite_output=True
-# python scripts/get_dfmdock_forces.py --config-name=testconf/dfmdock_traj_forces_2A1A.yaml ++overwrite_output=True
-# python scripts/sample_dfmdock.py --config-name=testconf/sample_dfmdock_nonoise.yaml ++overwrite_output=True
-
-# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_diff_trapezoid.yaml ++overwrite_output=True
-# python scripts/likelihood_gaussian_1d.py --config-name=testconf/gaussian_flow.yaml ++overwrite_output=True
-# python scripts/get_gaussian_forces.py --config-name=testconf/gaussian_traj_forces.yaml ++overwrite_output=True
-python scripts/sample_gaussian_1d.py --config-name=testconf/sample_gaussian_1d.yaml ++overwrite_output=True
--- user_scripts/test_classes_slurm.sh
+++
@@ -1,9 +0,0 @@
-# bash user_scripts/gpu_configurable.sh likelihood_dfmdock.py testconf/dfmdock_diff_trapezoid.yaml   &
-# bash user_scripts/gpu_configurable.sh likelihood_dfmdock.py testconf/dfmdock_flow_40.yaml   &
-# bash user_scripts/gpu_configurable.sh get_dfmdock_forces.py testconf/dfmdock_traj_forces_2A1A.yaml   &
-# bash user_scripts/gpu_configurable.sh sample_dfmdock.py testconf/sample_dfmdock_nonoise.yaml   &
-
-bash user_scripts/gpu_configurable.sh likelihood_gaussian_1d.py testconf/gaussian_diff_trapezoid.yaml   &
-# bash user_scripts/gpu_configurable.sh likelihood_gaussian_1d.py testconf/gaussian_flow.yaml   &
-# bash user_scripts/gpu_configurable.sh get_gaussian_forces.py testconf/gaussian_traj_forces.yaml   &
-# bash user_scripts/gpu_configurable.sh sample_gaussian_1d.py testconf/sample_gaussian_1d.yaml   &
