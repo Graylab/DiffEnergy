4471baad1c7d0c2df01cf5523689f915ad645dd8
htruscott <harrison@harriscott.net>
2025-12-16 15:15:31 -0500

    figure improvements

--- figures/figure5.py
+++ figures/figure5.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from matplotlib import pyplot as plt
 from gridplots import load_dfmdock_stats, plot_samples
 from shared import setfont
@@ -5,6 +6,9 @@ import itertools
 
 if __name__ == "__main__":
     setfont()
+    
+    outdir = Path("figures/figure5_subs")
+    outdir.mkdir(exist_ok=True)
     
     # Figure 5 Subfigures
     samples_source = 'dfmdock_inference_trtrained_deterministic'
@@ -26,6 +30,6 @@ if __name__ == "__main__":
                         custom_xlabel=labels[x],custom_ylabel=labels[y],
                         custom_xlim=limits[x],custom_ylim=limits[y],save=False)
             name = f"{id}_{y}_v_{x}"
-            f.savefig(f'figures/figure5_subs/{name}.png',dpi=300,bbox_inches='tight')
+            f.savefig(outdir/f'{name}.png',dpi=300,bbox_inches='tight')
             f.suptitle(name)
     
