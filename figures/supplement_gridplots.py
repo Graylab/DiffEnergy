5b6f4018a1c05e0ea22ce4434b542d534a6df4da
htruscott <harrison@harriscott.net>
2025-12-17 12:04:35 -0500

    fix massive plotting overhead for figures

--- figures/supplement_gridplots.py
+++ figures/supplement_gridplots.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     samples_source = 'dfmdock_inference_trtrained_deterministic'
     sample_stats, gt_stats, labels, limits = load_dfmdock_stats(samples_source)
     plot_all_grids(sample_stats,gt_stats,labels,limits,save=True,
-                   outdir=Path('figures/supplement_gridplots/'),close_plots=True,
-                   extra_pairs=[('diff_piecewise_ode_nll','diff_trapezoid_nll'),('diff_piecewise_ode_nll','flow_nll'),('diff_trapezoid_nll','flow_nll'),])
+                   outdir=Path('figures/supplement_gridplots/'),close_plots=True)
