66e7634fb0273fc29f52e99ed7992fdd08a6b90c
htruscott <harrison@harriscott.net>
2026-04-07 15:39:53 -0400

    cleanup figure 3 code
    Signed-off-by: htruscott <harrison@harriscott.net>

--- figures/figure3.py
+++ figures/figure3.py
@@ -1,13 +1,11 @@
 from matplotlib.figure import SubFigure
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
-import pickle
 import numpy as np
 import seaborn as sns
 from pathlib import Path
 from typing import Callable, Literal
-from scipy.stats import norm, laplace
+from scipy.stats import norm
 from scipy.stats import pearsonr
 from matplotlib.ticker import FuncFormatter
 from scipy.ndimage import gaussian_filter1d
@@ -58,6 +56,7 @@ def plot_sample_result(parent_folder:str|Path,
                        plot_p1_gt:bool=True,
                        legend:bool=False,
                        samples_color='salmon',
+                       samples_front=False,
                        plot_value:Literal["probability","energy"]="probability"):
     parent_folder = Path(parent_folder)
     out_subfolder = out_subfolder or likelihood_subfolder
@@ -86,12 +85,10 @@ def plot_sample_result(parent_folder:str|Path,
 
     # Plot the samples data
     samples_df = pd.read_csv(samples_file, index_col=0)
-    samples_1 = samples_df.values.flatten()
+    samples = samples_df.values.flatten()
     
     # Load the likelihoods
-    filename_2 = likelihood_file
-    likelihood_df = pd.read_csv(filename_2)
-    samples_1 = samples_df.loc[likelihood_df['id']].values.flatten()
+    likelihood_df = pd.read_csv(likelihood_file).loc[samples_df.index]
     
     likelihoods = likelihood_df[integrand_column] + likelihood_df[prior_column]
     if likelihood_offset is not None:
@@ -102,7 +99,7 @@ def plot_sample_result(parent_folder:str|Path,
         likelihoods += likelihood_offset.loc[likelihood_df['id']]
     likelihoods = likelihoods.values.flatten()
 
-    sigma_min, sigma_max = 0.1, 30.0  # Define sigma limits
+    sigma_min, sigma_max = 0.1, 70.0  # Define sigma limits
     gaussian_pdf = get_gt_gaussian(x)
 
     if plot_value == "probability":
@@ -111,29 +108,30 @@ def plot_sample_result(parent_folder:str|Path,
             #plt.fill_between(x, gaussian_pdf, color='pink', alpha=0.6)
 
         if plot_p1_gt:
-            sigma = marginal_prob_std(1, sigma_min, sigma_max)
+            from diffenergy.helper import int_diffusion_coeff_sq
+            sigma = np.sqrt(int_diffusion_coeff_sq(1, sigma_min, sigma_max))
             gaussian = norm.pdf(x, 0, sigma)  # Zero mean, std = sigma_t1
             ax.plot(x, gaussian, color='gray', linestyle='dashed', alpha=0.8, label="Prior $p_1(x_1)$", zorder = 2)
             #plt.fill_between(x, gaussian, color='green', alpha=0.2)
 
         if plot_kde:
-            sns.kdeplot(samples_1, color='steelblue', alpha=0.8, label="Sample KDE", zorder = 1, ax=ax, bw_adjust=0.3)
+            sns.kdeplot(samples, color='steelblue', alpha=0.8, label="Sample KDE", zorder = 1, ax=ax, bw_adjust=0.3)
 
         if plot_samples:
             probability = np.exp(likelihoods) #this was negative likelihoods in the old data since they calculated nll instead of ll
-            ax.scatter(samples_1, probability, color=samples_color, label="Learned $p_0(x_0)$", s=2, linewidth=0.1, edgecolors=(0.1, 0.1, 0.1), alpha=0.8, zorder=-1)
+            ax.scatter(samples, probability, color=samples_color, label="Learned $p_0(x_0)$", s=2, linewidth=0.1, edgecolors=(0.1, 0.1, 0.1), alpha=0.8, zorder=10 if samples_front else -1)
             
             if binline:
                 clamp_index = np.where(probability < 0.03)[0]
                 nbins = 150
-                bin_centers,prob_mean = get_binline(nbins,samples_1[clamp_index],probability[clamp_index])
+                bin_centers,prob_mean = get_binline(nbins,samples[clamp_index],probability[clamp_index])
                 ax.plot(bin_centers, prob_mean, color=binline_color, zorder = 3, alpha=0.5)
 
         mu1, mu2, mu3 = -30, 0, 40
         b1, b2, b3 = 8.0, 5.0, 10.0
         w1, w2, w3 = 0.4, 0.3, 0.3
         gaussian_cdf =lambda x: (w1 * norm.cdf(x, loc=mu1, scale=b1) + w2 * norm.cdf(x, loc=mu2, scale=b2) + w3 * norm.cdf(x, loc=mu3, scale=b3))
-        res = kstest(samples_1, gaussian_cdf)
+        res = kstest(samples, gaussian_cdf)
         print("kde match:", res)
 
         
@@ -182,19 +180,18 @@ def plot_sample_result(parent_folder:str|Path,
 
         #getting a logarithmic kde is annoying, have to do it manually
         if plot_kde:
-            from scipy.stats import gaussian_kde
-            kde = gaussian_kde(samples_1)
+            kde = gaussian_kde(samples)
             kde.set_bandwidth(kde.factor * 0.3) #bw_adjust from sns kde for parity with above
             ax.plot(x,-kde.logpdf(x), color='steelblue', alpha=0.8, label="-log(Density) estimate from generated samples", zorder = 1)
             # sns.kdeplot(samples_1, color='steelblue', alpha=0.8, label="Density estimate from generated samples", zorder = 1, ax=ax, bw_adjust=0.3)
 
         if plot_samples:
             energy = -likelihoods
-            ax.scatter(samples_1, energy, color=samples_color, label='nll', s=2, linewidth=0.1, edgecolors=(0.1,0.1,0.1), alpha=0.8, zorder=-1)
+            ax.scatter(samples, energy, color=samples_color, label='nll', s=2, linewidth=0.1, edgecolors=(0.1,0.1,0.1), alpha=0.8, zorder=-1)
 
             if binline:
                 nbins = 150
-                bin_centers,prob_mean = get_binline(nbins,samples_1,energy)
+                bin_centers,prob_mean = get_binline(nbins,samples,energy)
                 ax.plot(bin_centers, prob_mean, color=binline_color, zorder = 3, alpha=0.5)
 
         # Labels and title
@@ -217,6 +214,8 @@ def plot_sample_result(parent_folder:str|Path,
         outfolder.mkdir(exist_ok=True,parents=True)
         plt.savefig(outfolder/(out_filename or f"{integrand_column}_{plot_value}.png"), dpi=300)
 
+    return fig, ax
+
 
 def plot_correlation(parent_folder:str|Path, likelihood_subfolder:str|Path, integrand_column:str, prior_column:str, out_subfolder:str|Path|None=None, out_filename:str|None=None, samples_file:Optional[str|Path]=None, ax_title:str|bool=True, fig_title:str|bool=True, ax:Optional[Axes]=None, save:bool=False, likelihood_offset=None):
     parent_folder = Path(parent_folder)
@@ -231,12 +230,10 @@ def plot_correlation(parent_folder:str|Path, likelihood_subfolder:str|Path, inte
 
     
     # Plot the samples data
-    filename_1 = samples_file
-    samples_df = pd.read_csv(filename_1, index_col=0)
-    samples_1 = samples_df.values.flatten()
+    samples_df = pd.read_csv(samples_file, index_col=0)
+    samples = samples_df.values.flatten()
 
-    filename_2 = likelihood_file
-    likelihood_df = pd.read_csv(filename_2)
+    likelihood_df = pd.read_csv(likelihood_file)
     likelihoods = likelihood_df[integrand_column] + likelihood_df[prior_column]
     if likelihood_offset is not None:
         if isinstance(likelihood_offset,str):
@@ -246,9 +243,9 @@ def plot_correlation(parent_folder:str|Path, likelihood_subfolder:str|Path, inte
         likelihoods += likelihood_offset.loc[likelihood_df['id']]
     likelihoods = likelihoods.values.flatten()
     probability = np.exp(likelihoods) #this was negative likelihoods in the old data since they calculated nll instead of ll
-    samples_1 = samples_df.loc[likelihood_df['id']].values.flatten() #only get samples for whom the likelihood has been calculated
+    samples = samples_df.loc[likelihood_df['id']].values.flatten() #only get samples for whom the likelihood has been calculated
 
-    gaussian_pdf = get_gt_gaussian(samples_1)
+    gaussian_pdf = get_gt_gaussian(samples)
 
 
     if not ax:
