3b05dda66e382fbdaef036dcaeef8b95aea9c24a
htruscott <harrison@harriscott.net>
2026-04-17 11:31:33 -0400

    supplement figure progress

--- figures/supplement_integration_method.py
+++ figures/supplement_integration_method.py
@@ -1,15 +1,20 @@
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+from matplotlib.figure import SubFigure
+
 try:
     from shared import setfont
-    from figure3 import plot_sample_result, 
+    from figure3 import plot_sample_result, plot_correlation
 except ImportError:
     from figures.shared import setfont
-
+    from figures.figure3 import plot_sample_result, plot_correlation
 
 if __name__ == "__main__":
     setfont()
 
     # Figure 3
-    likelihood_dir = Path('results/likelihood')
+    likelihood_dir = Path('results/likelihood/diff_integration_methods')
 
     #going to try using subfigures. Hope this works!
     def add_comb_row(fig:SubFigure, parent_folder:Path, likelihood_subfolder:Path, integrand:str, prior:str, title:str, label:str, binline=False,):
@@ -25,9 +30,16 @@ if __name__ == "__main__":
         #add subfigure label
         fig.text(.01, .99, label, ha='left', va='top', transform=fig.transSubfigure,fontdict={"fontsize":"large"})
 
-    f = plt.figure(figsize=(5,4),layout='constrained')
-    subfs:list[SubFigure] = f.subfigures(nrows=2,wspace=0.5,squeeze=True)
-    add_comb_row(subfs[0],likelihood_dir,Path('gaussian_1d_diff'),'integrand:TotalIntegrand','prior:smax_gaussian','Diffusion Trajectory','(a)',binline=True)
-    add_comb_row(subfs[1],likelihood_dir,Path('gaussian_1d_flow'),'integrand:TotalIntegrand','prior:smax_gaussian','Flow Trajectory','(b)',binline=True)
+    f = plt.figure(figsize=(5,14),layout='constrained')
+    subfs:list[SubFigure] = f.subfigures(nrows=7,wspace=0.5,squeeze=True)
+    
+    add_comb_row(subfs[0],likelihood_dir,Path('gaussian_1d_diff_forward_euler'),'integrand:TotalIntegrand','prior:smax_gaussian','Forward Euler Integration','(a)',binline=True)
+    add_comb_row(subfs[1],likelihood_dir,Path('gaussian_1d_diff_forward_euler_interpolated'),'integrand:TotalIntegrand','prior:smax_gaussian','Forward Euler Integration,\n10x Interpolated Trajectory','(b)',binline=True)
+    add_comb_row(subfs[2],likelihood_dir,Path('gaussian_1d_diff_backward_euler'),'integrand:TotalIntegrand','prior:smax_gaussian','Backward Euler Integration','(c)',binline=True)
+    add_comb_row(subfs[3],likelihood_dir,Path('gaussian_1d_diff_backward_euler_interpolated'),'integrand:TotalIntegrand','prior:smax_gaussian','Backward Euler Integration,\n10x Interpolated Trajectory','(d)',binline=True)
+    add_comb_row(subfs[4],likelihood_dir,Path('gaussian_1d_diff_trapezoid'),'integrand:TotalIntegrand','prior:smax_gaussian','Trapezoidal Integration','(e)',binline=True)
+    add_comb_row(subfs[5],likelihood_dir,Path('gaussian_1d_diff_trapezoid_interpolated'),'integrand:TotalIntegrand','prior:smax_gaussian','Trapezoidal Integration,\n10x Interpolated Trajectory','(f)',binline=True)
+    add_comb_row(subfs[6],likelihood_dir,Path('gaussian_1d_diff_piecewise_ode'),'integrand:TotalIntegrand','prior:smax_gaussian','Piewise ODE Integration','(g)',binline=True)
 
-    f.savefig("figures/figure_3.png",dpi=600)
+
+    f.savefig("figures/supplement_integration_method.png",dpi=600)
