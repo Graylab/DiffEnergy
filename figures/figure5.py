e193c50c95746c64b4729c9f474973026de841d3
htruscott <harrison@harriscott.net>
2026-05-18 13:32:40 -0400

    add diffusion/ensembled to figs and add smax gaussian prior
    Signed-off-by: htruscott <harrison@harriscott.net>

--- figures/figure5.py
+++ figures/figure5.py
@@ -13,19 +13,25 @@ except ImportError:
 if __name__ == "__main__":
     setfont()
     
+    # outdir = Path("figures/figure5_subs")
     outdir = Path("figures/figure5_subs")
     outdir.mkdir(exist_ok=True)
     
     # Figure 5 Subfigures
-    sample_stats, gt_stats, labels, limits = load_dfmdock_stats()    
+    # sample_stats, gt_stats, labels, limits = load_dfmdock_stats()  
+    sample_stats, gt_stats, labels, limits = load_dfmdock_stats(prior='receptor_smax_gaussian')
 
-    y = ['flow_nll','rosetta_Isc']
+    y = ['flow_nll','diff_nll','diff_15ensembled_nll','rosetta_Isc']
     x = ['DockQ','i_rmsd']
+
+    limits['diff_nll'] = None
 
     pairs = list(itertools.product(x,y))
 
     for id in ('2SIC','2A1A'):
         print(id)
+        # if id == '2A1A':
+        #     limits['diff_nll'] = (-5,7)
         for x,y in pairs:
             f,ax = plt.subplots(figsize=(2.5,2))
             try:
@@ -34,7 +40,11 @@ if __name__ == "__main__":
                             custom_xlim=limits[x],custom_ylim=limits[y],save=False)
             except KeyError:
                 pass
+            if 'flow' in y:
+                ax.yaxis.labelpad = 13 #shift flow label away from xaxis so spacing is same as diff and rosetta w/ negative number ticklabels
+            if 'diff' in y and id == '2SIC':
+                ax.yaxis.labelpad = 5 #shift flow label away from xaxis so spacing is same as diff and rosetta w/ negative number ticklabels
             name = f"{id}_{y}_v_{x}"
-            f.savefig(outdir/f'{name}.png',dpi=300,bbox_inches='tight')
+            f.savefig(outdir/f'{name}.png',dpi=600,bbox_inches='tight')
             f.suptitle(name)
     
